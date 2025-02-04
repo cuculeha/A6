d9f87a208cb4dec096613ad577df92b12ba2a089
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-02-04 14:15:09 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -234,6 +234,8 @@ void printTeam (const playerType list [])
 			printPlayer (list[i].name, list[i].position, list[i].numTouchDowns, list [i].numCatches, list[i]. numPassing,
 				list[i].numReceiving, list[i].numRushing);
 		}
+	cout << "Press any key to continue....\n";
+	cin.ignore();
 	return;
 }
 
